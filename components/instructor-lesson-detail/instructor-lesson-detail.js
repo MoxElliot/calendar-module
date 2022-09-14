7a6b353b3d7ba39adf7c70b047e7eca5d611b160
Elliot Erickson <eric0952@gmail.com>
2022-09-14 14:36:49 -0700

    Lesson Detail
    created lesson detail box

--- components/instructor-lesson-detail/instructor-lesson-detail.js
+++ components/instructor-lesson-detail/instructor-lesson-detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function InstructorLessonDetail({showLessonDet}) {
 
@@ -7,10 +8,43 @@ export default function InstructorLessonDetail({showLessonDet}) {
     } 
 
     return (
-        <div className="lesson-details">
-            <div >
-                <h4>Time Slot Details</h4>
+        <div className="lesson-details container">
+            <div className='row'>
+            <div className='lesson-text col col-md-6 col-12'>
+                <h4>Lesson Details</h4>
+                <p className='lesson-detail'>Game Review, Fundamentals Review</p>
+                <p className='lesson-detail'>John McStudent</p>
+                <p className='lesson-detail'>da/te/year at ti:me - ti:me</p>
+                <Link href="/">Discord Link</Link>
+            </div>
+            <div className="lesson-attachment m-2
+                col col-md-4 col-6
+                d-flex flex-column">
+                <h5 className='m-0'>Lesson Attachments</h5>
+                <button className='btn p-0'>
+                    <label className='px-2'>Add Attachment</label>
+                </button>
+                <Link href="/">
+                    <a className='bi bi-paperclip'>game-review.sgf</a>
+                </Link>
+                <Link href="/">
+                    <a className='bi bi-paperclip'>OpeningProblems.sgf</a>
+                </Link>
+            </div>
+            </div>
+            <div className="row">
+                <div className='col-8'>
+                    <button className='btn bi bi-pencil'>
+                        <label className='px-2'>Edit Lesson</label>
+                    </button>
+                </div>
+                <div className='col-8'>
+                    <button className='btn bi bi-trash '> 
+                        <label className='px-2'>Cancel Lesson</label>
+                    </button>
+                </div>
             </div>
         </div>
+
     )
 }
